d33096e8e1703e1281dccac83b9cf0cb0980bbc4
Patrick <Patrick@Patrick-Petros-MacBook-Pro.local>
2020-03-05 21:09:59 -0500

    Travis test

--- models/index.js
+++ models/index.js
@@ -28,7 +28,7 @@ Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-});
+}); 
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
