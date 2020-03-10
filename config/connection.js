38243eeccb24d56007fde3d9bf0c5a9ada7a7d32
Patrick Dunn <pmdunn78@gmail.com>
2020-03-10 18:40:41 -0400

    gitignore mysql password

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,13 @@
 
 // Dependencies
 var Sequelize = require("sequelize");
+var password = require("./../mySQLpass");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("time_veil_db", "root", "password", {
+var sequelize = new Sequelize("time_veil_db", "root", {
   host: "localhost",
   port: 3306,
+  password: password,
   dialect: "mysql",
   pool: {
     max: 5,
