12145d8a5a228d4c4502ff452eb27b89a659c75c
Sforzemon <sforzemon@gmail.com>
2020-03-10 18:48:22 -0400

    adding sign in and sign up logic to find values

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 
   email.on("keyup", function() {
     signupLink.addClass("hidden");
+    // console.log(email);
   });
     
   password.on("keyup", function() {
@@ -17,9 +18,37 @@ $(document).ready(function() {
       loginBtn.addClass("hidden");
       passPrompt.removeClass("hidden");
     }
+    // console.log(password);
   });
 
+  loginBtn.on("click", function(event) {
+    event.preventDefault();
+    var userData = {
+      email: $("#enter-email").val(),
+      password: $("#enter-password").val()
+    };
+    console.log(userData.email, userData.password);
+    if (!userData.email || !userData.password) {
+      return;
+    }
+    loginUser(userData.email, userData.password);
+    email.val("");
+    password.val("");
+  });
 
+  function loginUser(emailInput, passwordInput) {
+    $.post("/api/login", {
+      email: emailInput,
+      password: passwordInput
+    })
+      .then(function() {
+        window.location.replace("/members");
+        // If there's an error, log the error
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
 //   select login button
 //   listener for click
 //      capture val of username input
