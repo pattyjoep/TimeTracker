0d88198540ace516afd02268b664eb3417344aee
Sforzemon <sforzemon@gmail.com>
2020-03-10 18:51:40 -0400

    adding logic to sign in and sign up.js

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,25 +1,78 @@
+/* eslint-disable camelcase */
 /* eslint-disable indent */
-$(document).on("ready", function() {
+$(function() {
     
-    // nested list start --------------------------------------------
+  // nested list start --------------------------------------------
+  $(".list-group-item").on("click", function() {
+      $(".glyphicon", this)
+        .toggleClass("glyphicon-chevron-right")
+        .toggleClass("glyphicon-chevron-down");
+    });
+  // nested list end ----------------------------------------------
 
-    $(".list-group-item").on("click", function() {
-        $(".glyphicon", this)
-          .toggleClass("glyphicon-chevron-right")
-          .toggleClass("glyphicon-chevron-down");
-      });
+  // creating a new task
+  $(".add-task-btn").on("click", function(event) {
+    event.preventDefault();
+    var task_name = $(".add-task-input").val().trim();
+    console.log("task_name:" + task_name);
 
+    var newTask = {
+      name: task_name
+    };
 
+    $.ajax("/api/tasks", {
+      type: "POST",
+      data: newTask
+    }).then(
+      function() {
+        console.log("created new task" + newTask);
+        location.reload();
+      }
+    );
+  });
 
+  // creating a new project
+  $(".add-project-btn").on("click", function(event) {
+    event.preventDefault();
+    var project_name = $(".add-project-input").val().trim();
+    console.log("project_name:" + project_name);
 
+    var newProject = {
+      name: project_name
+    };
 
+    $.ajax("/api/projects", {
+      type: "POST",
+      data: newProject
+    }).then(
+      function() {
+        console.log("created new project" + newProject);
+        location.reload();
+      }
+    );
+  });
 
+  // creating a new client
+  // add modal for inputting all info
+  $(".add-client-btn").on("click", function(event) {
+    event.preventDefault();
+    var client_name = $(".add-client-input").val().trim();
+    console.log("client_name:" + client_name);
 
+    var newClient = {
+      name: client_name
+    };
 
-
-    
-    // nested list end ----------------------------------------------
-
+    $.ajax("/api/clients", {
+      type: "POST",
+      data: newClient
+    }).then(
+      function() {
+        console.log("created new client" + newClient);
+        location.reload();
+      }
+    );
+  });
 
 
 
@@ -34,14 +87,3 @@ $(document).on("ready", function() {
     
 
 });
-
-
-// var toggler = document.getElementsByClassName("caret");
-// var i;
-
-// for (i = 0; i < toggler.length; i++) {
-//   toggler[i].addEventListener("click", function() {
-//     this.parentElement.querySelector(".nested").classList.toggle("active");
-//     this.classList.toggle("caret-down");
-//   });
-// }
