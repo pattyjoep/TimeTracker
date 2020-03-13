9bebcf8f941e3e4d6890e8ac7749f3a5eac2c9a9
Patrick <Patrick@Patrick-Petros-MacBook-Pro.local>
2020-03-13 14:31:19 -0400

    routes

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Find all Tasks and return them to the user with res.json
+  // Find all users and return them with res.json
   app.get("/api/users", function(req, res) {
     db.Users.findAll({}).then(function(dbUsers) {
       res.json(dbUsers);
@@ -9,7 +9,7 @@ module.exports = function(app) {
   });
 
   app.get("/api/users/:id", function(req, res) {
-    // Find one Author with the id in req.params.id and return them to the user with res.json
+    // Find one user with the id in req.params.id and return them to the user with res.json
     db.Users.findOne({
       where: {
         id: req.params.id
